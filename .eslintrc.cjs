597c97eb2db2731ad5a2359817a6fdfb89584958
sarvagnakadiya <sarvagnakadiya1@gmail.com>
2023-09-05 22:19:02 +0530

    build errors solved

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,37 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+  },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser", // Specify TypeScript parser
+  parserOptions: {
+    ecmaVersion: 2020, // Use a specific ECMAScript version
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true, // Enable JSX support
+    },
+  },
+  settings: {
+    react: {
+      version: "18.2",
+    },
+  },
+  plugins: ["react", "react-hooks", "@typescript-eslint"], // Add TypeScript ESLint plugin
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "no-unused-vars": "off",
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "react/prop-types": "off", // Disable prop-type validation
+    "@typescript-eslint/no-unused-vars": "off", // Disable TypeScript unused variable check
   },
-}
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { App } from "./App.tsx";
 import "./index.css";
-import React, { useMemo, useState } from "react";
+import React from "react";
 import type {
   Adapter,
   WalletError,
@@ -10,20 +10,15 @@ import {
   WalletDisconnectedError,
   WalletNotFoundError,
 } from "@tronweb3/tronwallet-abstract-adapter";
-import {
-  WalletProvider,
-} from "@tronweb3/tronwallet-adapter-react-hooks";
-import {
-  WalletModalProvider,
-} from "@tronweb3/tronwallet-adapter-react-ui";
+import { WalletProvider } from "@tronweb3/tronwallet-adapter-react-hooks";
+import { WalletModalProvider } from "@tronweb3/tronwallet-adapter-react-ui";
 import toast from "react-hot-toast";
 import { TronLinkAdapter } from "@tronweb3/tronwallet-adapters";
-
 
 function onError(e: WalletError) {
   console.log(e);
   if (e instanceof WalletNotFoundError) {
-    alert(e.message)
+    alert(e.message);
     toast.error(e.message);
   } else if (e instanceof WalletDisconnectedError) {
     toast.error(e.message);
