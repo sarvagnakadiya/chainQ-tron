0e21c8f25743e99e46b39f42a6c988db6f75c422
sarvagnakadiya <sarvagnakadiya1@gmail.com>
2023-09-23 20:56:03 +0530

    APIs redirected to ChainQ apis

--- src/APIs/apis.js
+++ src/APIs/apis.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 // const API_BASE_URL = "http://localhost:3002/";
-const API_BASE_URL = "https://chainq.lampros.tech:3002/";
+const API_BASE_URL = "https://chainq.lampros.tech/";
 
 // const API_BASE_URL = "https://chainq.lampros.tech/";
 
