619fe9ed309141abe8c070f71040a6e3db6ec727
purvik6062 <purvikpanchal6336@gmail.com>
2023-09-24 01:43:37 +0530

    changed api base url to hosted

--- src/APIs/apis.js
+++ src/APIs/apis.js
@@ -2,8 +2,8 @@
 
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3002/";
-// const API_BASE_URL = "http://chainq.lampros.tech:3002/";
+// const API_BASE_URL = "http://localhost:3002/";
+const API_BASE_URL = "http://chainq.lampros.tech:3002/";
 
 // const API_BASE_URL = "https://chainq.lampros.tech/";
 
